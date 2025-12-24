4b1e6e23fbf8f07bcec1c2e58c7788ed844bb8cd
Lucas Meng <mengliang@deeprobotics.cn>
2025-12-24 15:27:04 +0800

    [fix] reduce pre-enqueue seeds in flood-filling

--- src/elevation_map.cpp
+++ src/elevation_map.cpp
@@ -614,23 +614,23 @@ void ElevationMap::judgePassability(float rough_thres, float drop_thres,
     enqueue_seed_index(idx.x(), idx.y());
   };
 
-  // the starting point of flood-filling (BFS)
-  const int center_x = size_x / 2;
-  const int center_y = size_y / 2;
-  enqueue_seed_index(center_x, center_y);
-  // manually add a point located at the front
-  int front_x = std::min(static_cast<int>(center_x * 0.3f), size_x - 1);
-  int front_y = std::min(static_cast<int>(center_y * 0.8f), size_y - 1);
-  enqueue_seed_index(front_x, front_y);
-  // manually add a point located at the back
-  int back_x = std::min(static_cast<int>(center_x * 1.7f), size_x - 1);
-  int back_y = std::min(static_cast<int>(center_y * 1.2f), size_y - 1);
-  enqueue_seed_index(back_x, back_y);
-  // manually add points at metric positions relative to map center
-  enqueue_seed_position(0.4f, 0.0f);
-  enqueue_seed_position(-0.4f, 0.0f);
-  enqueue_seed_position(0.0f, 1.0f);
-  enqueue_seed_position(0.0f, -1.0f);
+  // // the starting point of flood-filling (BFS)
+  // const int center_x = size_x / 2;
+  // const int center_y = size_y / 2;
+  // enqueue_seed_index(center_x, center_y);
+  // // manually add a point located at the front
+  // int front_x = std::min(static_cast<int>(center_x * 0.3f), size_x - 1);
+  // int front_y = std::min(static_cast<int>(center_y * 0.8f), size_y - 1);
+  // enqueue_seed_index(front_x, front_y);
+  // // manually add a point located at the back
+  // int back_x = std::min(static_cast<int>(center_x * 1.7f), size_x - 1);
+  // int back_y = std::min(static_cast<int>(center_y * 1.2f), size_y - 1);
+  // enqueue_seed_index(back_x, back_y);
+  // // manually add points at metric positions relative to map center
+  enqueue_seed_position(0.45f, 0.0f);
+  enqueue_seed_position(-0.45f, 0.0f);
+  // enqueue_seed_position(0.0f, 1.0f);
+  // enqueue_seed_position(0.0f, -1.0f);
 
   const int dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
   const int dy[8] = {0, -1, -1, -1, 0, 1, 1, 1};
